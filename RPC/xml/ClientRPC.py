09c4ea50a62e6e07df7f0b34302aaae0e688ab69
Lavinia Medeiros <laviniameds@icloud.com>
2018-08-29 21:07:03 -0300

    RPC feito

---
+++ RPC/xml/ClientRPC.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import xmlrpc.client #importa a biblioteca RPC xml do lado cliente
+
+with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy: #define um proxy
+    print("3 é par: %s" % str(proxy.is_even(3))) #chama a função que está definida no servidor passando o parametro
+    print("100 é par: %s" % str(proxy.is_even(100)))
---
+++ RPC/xml/ServerRPC.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from xmlrpc.server import SimpleXMLRPCServer #importa a biblioteca de RPC com XML do lado servidor
+
+def is_even(n):
+    print("Requisição recebida com o seguinte argumento: " + str(n))
+    return n % 2 == 0
+
+server = SimpleXMLRPCServer(("localhost", 8000)) #define a porta do servidor
+print("Escutando na porta 8000...")
+server.register_function(is_even, "is_even") #recebe a funcao is_even com parametros
+server.serve_forever() #define que o servidor vai receber requisições enquato não houver uma exceção (shutdown)
